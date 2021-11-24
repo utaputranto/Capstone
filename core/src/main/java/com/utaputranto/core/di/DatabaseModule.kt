5df02467db48fee76b0d6a021dc2ffe2c0a494b3
Putra <utaputranto@outlook.com>
2021-11-25 01:09:03 +0700

    add sql cipher

--- core/src/main/java/com/utaputranto/core/di/DatabaseModule.kt
+++ core/src/main/java/com/utaputranto/core/di/DatabaseModule.kt
@@ -7,6 +7,8 @@ import com.utaputranto.core.data.source.local.room.MovieTvDao
 import com.utaputranto.core.data.source.local.room.MovieTvDatabase
 import dagger.Module
 import dagger.Provides
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -14,10 +16,16 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(context: Context): MovieTvDatabase = Room.databaseBuilder(
-        context,
-        MovieTvDatabase::class.java, "MovieTvLocal.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(context: Context): MovieTvDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
+            context,
+            MovieTvDatabase::class.java, "MovieTvLocal.db"
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
 
     @Provides
     fun provideMovieTvDao(database: MovieTvDatabase): MovieTvDao = database.movieTvDao()
